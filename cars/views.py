8921bd3df096f616c370dc4f74308bb1056676d3
Hot Mama <hotmama@Hots-MBP.hsd1.or.comcast.net>
2022-07-13 15:42:40 -0700

    completed delete functionality for car views file

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,5 @@
 
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,15 +23,19 @@ def cars_list (request):
         return Response (serializer.data,status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def car_detial(request,pk):
-    try:
-        car=Car.objects.get(pk=pk)   
-        serializer= CarSerializer(car)
+    car= get_object_or_404(Car, pk=pk) 
+    if request.method == 'GET':
+        serializer= CarSerializer(Car, pk=pk)
         return Response(serializer.data)
 
-    except  Car.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND);   
-    
-
+    elif request.method == 'PUT':  
+      serializer = CarSerializer(car, data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data)
+    elif request.method == 'DELETE':  
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
